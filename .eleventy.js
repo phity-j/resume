db071fea5bc94813757e627b031ee0c26f6a85bd
phity <phitouch@gmail.com>
2026-07-23 10:12:15 +0900

    Fix path routing for GitHub Pages hosting

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
 
   return {
+    pathPrefix: "/resume/",
     dir: {
       input: "src",
       output: "_site",
